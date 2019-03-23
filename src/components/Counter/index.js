343dcfa6664248f13fa9942a85e5288e86efc934
Evgeny Myltsev <myltsev.ea@gmail.com>
2019-03-23 12:51:20 +0300

    setState as func

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 import './index.css';
 
+function increment(state, props) {
+    return { count: state.count + props.step };
+}
+
 class Counter extends React.Component {
     state = {
         count: 0
     };
 
-    increment = () => this.setState(state => ({ count: state.count + 1 }));
+    increment = () => {
+        this.setState(increment);
+    };
     
     decrement = () => this.setState(state => ({ count: state.count - 1 }));
 
