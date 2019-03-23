703664f978eb6392e7076979a275f525936617ed
Evgeny Myltsev <myltsev.ea@gmail.com>
2019-03-23 14:43:28 +0300

    add control props

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import React from 'react';
 import Counter from './components/Counter';
 
 export default class App extends React.Component {
+    state = {
+        count: 0
+    }
+
+    handleChange = count => this.setState(state => ({ count }))
+
     render() {
         return (
-            <Counter />
+            <React.Fragment>
+                <Counter onChange={this.handleChange}/>
+
+                <Counter count={this.state.count} onChange={this.handleChange}/>
+            </React.Fragment>
+
         );
     }
 }
